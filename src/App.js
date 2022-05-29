3e584a8126890de327398cc4797694ec9cb0f582
Dada Khalandar <kerry.blig12@gmail.com>
2022-05-29 19:46:52 +0530

    added pic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import { Text } from '@chakra-ui/react'
+import { Text, Image } from '@chakra-ui/react'
+import john from './assets/john lee.webp'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Text>Please Donate to the Zhongli No Mora No Roti</Text>
         <Text>Website is coming soon</Text>
+        <Image src={john} boxSize='20vw'
+          objectFit='cover' />
 
       </header>
     </div>
