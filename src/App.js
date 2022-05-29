89e7432176c8bda64a839c4d731c14a55cf5ed2a
Dada Khalandar <kerry.blig12@gmail.com>
2022-05-29 19:40:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Text } from '@chakra-ui/react'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Text>Please Donate to the Zhongli No Mora No Roti</Text>
+        <Text>Website is coming soon</Text>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
